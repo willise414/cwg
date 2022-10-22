f3bacfd15df2cc755d1c62a9b850ad04e6baf806
Sean Williams <willise@nbnet.nb.ca>
2022-10-22 17:22:26 -0300

    Updates

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+include "assets/inc/header.php"
+
+?>
+
+<!-- SHOWCASE AREA -->
+
+<div class="showcase">
+  <div class="banner img-fluid">
+    <div class="container">
+      <h1 class="headline">FOR SENIORS 55+ TO LEARN AND PRACTICE WOODWORKING IN THE GREATER
+        MONCTON
+        AREA
+      </h1>
+    </div>
+  </div>
+</div>
+
+
+<!--Testimonial and Aim -->
+<section class="boxes">
+  <div class="container">
+    <div class="box">
+
+      <p>Whether you're an experienced woodworker who only needs to use a few tools, or just starting out in
+        woodworking and looking to learn more, all are welcome to become a member of the Codiac Woodworkers Guild.</p>
+    </div>
+    <div class="box">
+      <p>"Everyone I have met has been very friendly and welcoming. I was so impressed, that i convinced my husband
+        and
+        brother to also sign up!"</p>
+      <p class="signature">Bonnie Shaw</p>
+
+    </div>
+    <div class="box">
+      <p>It is our aim to promote and provide a welcoming environment for the practice of woodworking. We encourage
+        our members to tackle new projects and to share their knowledge with other members.</p>
+
+    </div>
+  </div>
+</section>
+<!-- end testimonial -->
+
+<!-- New Interested Section-->
+<section class="interested-boxes">
+  <div class="interested-image">
+    <div class="interested-container ">
+      <div class="grid">
+        <div class="location">
+          <h3>Interested in touring our shop?</h3>
+          <iframe style="border-radius: 10px; opacity: .7" tabindex="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2768.380389579085!2d-64.81964768396254!3d46.063455879112645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ca0b9ae611f8bd7%3A0xc6fe4320d2ce8d81!2s51%20Biggs%20Dr%2C%20Riverview%2C%20NB%20E1B%203Y8!5e0!3m2!1sen!2sca!4v1609346808896!5m2!1sen!2sca" width="100%" height="80%" frameborder="0" aria-hidden="false"></iframe>
+          <h5 class="locationLink"><a href="https://www.google.com/maps/place/16+Biggs+Dr,+Riverview,+NB+E1B+3H5/@46.0620034,-64.8175012,17z/data=!4m2!3m1!1s0x4ca0b9ae38bf18cd:0x9d19e54032219559">Click here to view our location</a></h5>
+        </div>
+        <div class="contact">
+          <h3>Interested in joining the guild?</h3>
+          <div class="card">
+            <form action="" class="card-form">
+              <div class="form-control">
+                <input type="text" id="name" name="name" placeholder="Name" required>
+              </div>
+              <div class="form-control">
+                <input type="email" id="email" name="email" placeholder="Email" required>
+              </div>
+              <div class="form-control">
+                <input type="text" id="address" name="address" placeholder="Address" required>
+              </div>
+              <div class="form-control">
+                <input type="text" id="city" name="city" placeholder="City" required>
+              </div>
+              <div class="form-control">
+                <input type="text" id="phone" name="phone" placeholder="Phone" required>
+              </div>
+              <div class="form-control">
+                <input type="checkbox" id="ageCheck" name="ageChech" class="ageCheck" required>
+                <label for="">I am 55 years of age or older</label>
+              </div>
+              <input type="submit" value="Submit" class="btn btn-primary">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
+
+<?php
+include "assets/inc/footer.php";
+?>
