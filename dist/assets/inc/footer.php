c38ee5dd0d401688ad9119f6909862086859db63
Sean Williams <willise@nbnet.nb.ca>
2022-10-22 17:19:29 -0300

    initial Commit

---
+++ dist/assets/inc/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+  <div class="container flex">
+    <p>Sponsored by the <a href="http://www.townofriverview.ca"> Town of Riverview</a></p>
+    <p>Codiac Woordworkers Guild &copy; 2022</p>
+    <p>Website design - Sean Williams</p>
+
+  </div>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ dist/assets/inc/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- TEsting with bootstrap -->
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Codiac Woodworkers Guild</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <link rel="stylesheet" href="assets/styles/styles-bs.css">
+</head>
+
+<body>
+
+  <!-- NAVBAR -->
+  <div class="navbar navbar-expand-lg">
+    <div class="container">
+      <a href="#" class="navbar-brand  navbar-img"><img src="assets/img/logo.png" alt=""></a>
+      <button type="button" class="navbar-toggler" data-bs-target="#navmenu" data-bs-toggle="collapse">
+        <span><i class="fas fa-bars"></i></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navmenu">
+
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a href="about.php" class="nav-link">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="facility.php" class="nav-link">Facility</a>
+          </li>
+          <li class="nav-item">
+            <a href="community.php" class="nav-link ">Community</a>
+          </li>
+          <li class="nav-item">
+            <a href="community.php" class="nav-link">Join</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Login</a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </div>
